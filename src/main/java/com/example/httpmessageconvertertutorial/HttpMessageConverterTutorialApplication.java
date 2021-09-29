ef418e1cfcc27c4fb76f4ed3efda4891956ee4f1
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-09-29 15:04:45 +0900

    init

---
+++ src/main/java/com/example/httpmessageconvertertutorial/HttpMessageConverterTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpmessageconvertertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpMessageConverterTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpMessageConverterTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/httpmessageconvertertutorial/HttpMessageConverterTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpmessageconvertertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpMessageConverterTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
